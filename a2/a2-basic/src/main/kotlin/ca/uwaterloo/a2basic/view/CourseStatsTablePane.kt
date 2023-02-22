400f7b4104a097436184e449d86ffb01a0bed6ef
Linus Zhang <l625zhan@uwaterloo.ca>
2023-02-22 12:49:36 -0500

    Fixed Regression

--- a2/a2-basic/src/main/kotlin/ca/uwaterloo/a2basic/view/CourseStatsTablePane.kt
+++ a2/a2-basic/src/main/kotlin/ca/uwaterloo/a2basic/view/CourseStatsTablePane.kt
@@ -3,12 +3,11 @@ package ca.uwaterloo.a2basic.view
 import ca.uwaterloo.a2basic.view.tabs.*
 import javafx.scene.control.Tab
 import javafx.scene.control.TabPane
-import javafx.scene.layout.StackPane
 
 object CourseStatsTablePane : TabPane() {
-    private val averagePlotStackPane = StackPane(TermAveragePlot)
+    private val averagePlotStackPane = TabStackPane(TermAveragePlot)
     private val degreeProgressPlotStackPane = TabStackPane(DegreeProgressPlot)
-    private val incrementalAveragePlotStackPane = StackPane(IncrementalTermAveragePlot)
+    private val incrementalAveragePlotStackPane = TabStackPane(IncrementalTermAveragePlot)
 
     init {
         tabs.apply {
